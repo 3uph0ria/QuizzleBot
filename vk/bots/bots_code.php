75c808ab7f0c63b51fb3e098a5e7752f6a555248
3uph0ria <s33rgey@bk.ru>
2021-09-24 19:17:18 +0300

    Add the ability to respond with numbers.

--- vk/bots/bots_code.php
+++ vk/bots/bots_code.php
@@ -1,13 +1,13 @@
 <?php
 
 //$start = microtime(true);
+
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bots-panel/include/classes/Database.php');
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bots-panel/include/classes/vk.php');
 require_once('config.php');
 $Database = new Database();
 $vkRequest = new vk();
 
-//$botUser = $Database->SelectBotUser(userLogin);
 require_once($_SERVER['DOCUMENT_ROOT'] . '/bots-panel/include/classes/simplevk/autoload.php'); // Подключение библиотеки
 
 use DigitalStar\vk_api\VK_api as vk_api; // Основной класс
@@ -18,15 +18,15 @@ $vk = vk_api::create(TOKEN, VERSION)->setConfirm(KEY);
 $data = json_decode(file_get_contents('php://input')); //Получает и декодирует JSON пришедший из ВК
 $test = $Database->GetTest(1);
 
-if($_POST['mailingText'])
-{
-    $users = $Database->GetBotUsers(userId);
-    for($i = 0; $i < Count($users); $i++)
-    {
-        $vk->sendMessage($users[$i]['peer_id'], $_POST['mailingText']);
-    }
-    header('Location: https://gs-och.ru/bots-panel/pages/mailing.php');
-}
+//if($_POST['mailingText'])
+//{
+//    $users = $Database->GetBotUsers(userId);
+//    for($i = 0; $i < Count($users); $i++)
+//    {
+//        $vk->sendMessage($users[$i]['peer_id'], $_POST['mailingText']);
+//    }
+//    header('Location: https://gs-och.ru/bots-panel/pages/mailing.php');
+//}
 
 $vk->sendOK(); //Говорим vk, что мы приняли callback
 
@@ -34,9 +34,10 @@ $message = mb_strtolower($data->object->message->text, 'utf-8');
 $button = json_decode($data->object->message->payload);
 $peer_id = $data->object->message->peer_id;
 
+$question = $Database->GetQuestions(1);
+
 if($message)
 {
-
     $user = $Database->GetUser($peer_id, 1);
     if(!$user)
     {
@@ -44,16 +45,33 @@ if($message)
         $Database->AddUser(1, $peer_id, $searchUser[0]['photo_50'], $searchUser[0]['first_name'] . ' ' . $searchUser[0]['last_name']);
     }
 
-    if($button->command && $message != 'начать')
+    if(($button->command && $message != 'начать') || ($message > 0 && $message < 5))
     {
-        $tmp = explode('_', $button->command);
-        $IdQuestion = $tmp[1];
-        $IdAnswer = $tmp[2];
+        if(is_numeric($message))
+        {
+            for($i = 0; $i < Count($question); $i++)
+            {
+                $userQuestion = $Database->GetUserQuestion($user['Id'], $question[$i]['Id']);
+                if(!$userQuestion)
+                {
+                    $answers = $Database->GetAnswers($question[$i]['Id']);
+                    for($j = 0; $j < Count($answers); $j++)
+                    {
+                        $Database->AddUserResult($user['Id'], 1, $question[$i]['Id'], $message - 1, date('Y-m-d H:i:s'), 1);
+                    }
+                    break;
+                }
+            }
+        }
+        else
+        {
+            $tmp = explode('_', $button->command);
+            $IdQuestion = $tmp[1];
+            $IdAnswer = $tmp[2];
 
-        $Database->AddUserResult($user['Id'], 1, $IdQuestion, $IdAnswer, date('Y-m-d H:i:s'), 1);
+            $Database->AddUserResult($user['Id'], 1, $IdQuestion, $IdAnswer, date('Y-m-d H:i:s'), 1);
+        }
     }
-
-    $question = $Database->GetQuestions(1);
 
     for($i = 0; $i < Count($question); $i++)
     {
